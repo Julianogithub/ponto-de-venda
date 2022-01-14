af68e173012d2615ff7da6141b8e2210f465b69c
Juliano Rocha <julianorocha.ti@gmail.com>
2022-01-13 21:24:21 -0300

    Progeto Ponto de Vendas

---
+++ src/principal/Carregador.java
@@ -0,0 +1,16 @@
+
+package principal;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+
+public class Carregador extends JDesktopPane{
+    Image img = new ImageIcon(getClass().getResource("/imagens/principal/pvGrande.png")).getImage();
+    @Override
+    public void paintChildren(Graphics g){
+        g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+        super.paintChildren(g);
+    }
+}
